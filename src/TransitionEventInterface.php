8082f66926898c0faf4dcd4f5cedc57be14944fa
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-29 17:23:08 +0200

    Added `TransitionEventInterface`

---
+++ src/TransitionEventInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Dhii\Events;
+
+use Dhii\State\StateAwareInterface;
+use Dhii\State\TransitionAwareInterface;
+use Dhii\Util\String\StringableInterface as Stringable;
+use Psr\EventManager\EventInterface;
+
+/**
+ * An event that is associated with a state machine transition.
+ *
+ * @since [*next-version*]
+ */
+interface TransitionEventInterface extends
+    EventInterface,
+    TransitionAwareInterface,
+    StateAwareInterface
+{
+    /**
+     * Retrieves the new state after (and if) the transition is applied.
+     *
+     * @since [*next-version*]
+     *
+     * @return string|Stringable The new state.
+     */
+    public function getNewState();
+
+    /**
+     * Indicates whether or not the transition associated with this event should be aborted.
+     *
+     * @since [*next-version*]
+     *
+     * @param bool $flag True to abort the transition, false otherwise.
+     */
+    public function abortTransition($flag);
+
+    /**
+     * Checks if the transition associated with this event instance has been aborted.
+     *
+     * @since [*next-version*]
+     *
+     * @return bool True if the transition is aborted, false if not.
+     */
+    public function isTransitionAborted();
+}
